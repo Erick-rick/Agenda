323595b8b53ea733fba18543c17330de53ed4464
Erick-Rick <henrique.eryck@gmail.com>
2022-11-08 22:29:58 -0300

    autentificação e validação do usuário

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -18,7 +18,34 @@ exports.register = async function(req, res){
             return;
         }
 
-        req.flash('sucess', 'Seu usuario foi criado com sucesso!');
+        req.flash('success', 'Seu usuario foi criado com sucesso!');
+        req.session.save(function(){
+            return res.redirect('back');
+        });
+
+        return res.send(login.errors);
+        
+    } catch (error) {
+        console.log(e);
+        return res.render('404');
+    } 
+};
+
+exports.login = async function(req, res){
+    try {
+        const login = new Login(req.body);
+        await login.login();
+    
+        if(login.errors.length > 0){
+            req.flash('erros', login.errors);
+            req.session.save(function(){
+                return res.redirect('back');
+            });
+            return;
+        }
+
+        req.flash('success', 'Você logou no sistema');
+        req.session.user = login.user;
         req.session.save(function(){
             return res.redirect('back');
         });
--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,7 @@
 //npm i validator
+// npm i bcryptjs  - Criptografia para senha
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 /*
 const mongoose = require('mongoose');
 
@@ -18,16 +20,45 @@ class Login {
     this.user = null;
   }
 
+  async login(){
+    this.valida();
+
+    if(this.errors.length > 0) return;
+
+    this.user = await LoginModel.findOne({ email: this.body.email });
+
+    if(!this.user) {
+      this.errors.push('Usuario não existe.')
+      return;
+    }
+    if(bcryptjs.compareSync(this.body.password, this.user.password)){
+      this.errors.push('Senha inválida!');
+      this.user = null;
+      return;
+    }
+  }
+
   async register(){
     this.valida();
-    if(this.errors.length > 0)
-    return;
-    try {
-      // this.user = await LoginModel.create(this.body);   
-    } catch (e) {
-      console.log(e)
-    }
+
+    if(this.errors.length > 0) return;
+
+    await this.userExists();
+
+    if(this.errors.length > 0) return;
+
+    const salt = bcryptjs.genSaltSync();
+    this.body.password =  bcryptjs.hashSync(this.body.password, salt);
+    
+    // this.user = await LoginModel.create(this.body);   
    
+  }
+
+  async userExists(){
+    this.user = await LoginModel.findOne({ email: this.body.email });
+
+    if(this.user) this.errors.push('Usuário já existe.');
+
   }
 
   //Validação de email e senha
